5ae35f65a0b0ab22ebe54da4d7fa50df41eb3376
kurth4cker <kurth4cker@gmail.com>
2025-02-24 16:00:49 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kurth4cker/serve-dir
+
+go 1.23
+
+require codeberg.org/kurth4cker/go-sample v0.10.1
---
+++ main.go
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package main
+
+import "codeberg.org/kurth4cker/go-sample"
+
+func main() {
+	sample.Helloln("world")
+}
