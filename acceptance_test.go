6c000d0acab5e5992a5a57f4f401d683231ede71
kurth4cker <kurth4cker@gmail.com>
2025-03-19 05:30:23 +0300

    main: add acceptance test

---
+++ acceptance_test.go
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
+
+package main_test
+
+import "testing"
+
+func TestHello(t *testing.T) {
+}
